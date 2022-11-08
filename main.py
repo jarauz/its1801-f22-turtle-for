ebd372bd5dcf198ba0a12f628fb5441178b8b9a0
Julio Arauz <jarauz@alumni.pitt.edu>
2022-11-08 14:21:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+This is the Template Repl for Python with Turtle.
+
+Python with Turtle lets you make graphics easily in Python.
+
+Check out the official docs here: https://docs.python.org/3/library/turtle.html
+"""
+
+import turtle
+
+casper = turtle.Turtle()
+
+for i in range(4):
+  print(i)
+  casper.forward(75)
+  casper.left(90)
